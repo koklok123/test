e32216781bb5ce7b8f4c05329aa04545e77798f1
koklok123 <lokgom774@gmail.com>
2024-09-05 15:21:40 +0600

    Update bots.py

--- bots.py
+++ bots.py
@@ -1,8 +1,9 @@
 import logging
 import random
 import asyncio
-from aiogram import Bot, Dispatcher, types
-
+from aiogram import Bot, Dispatcher, types, Router
+from aiogram.types import Message
+from aiogram.filters import Command
 
 API_TOKEN = '7350431741:AAFawplWEL4ArfIeleEUkzWQAdVVgAhVrZg'
 
@@ -10,14 +11,20 @@ logging.basicConfig(level=logging.INFO)
 
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot=bot)
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
+router = Router()
+
+dp = Dispatcher()
+
+
+dp.include_router(router)
+
+@router.message(Command("start"))
+async def start_command(message: Message):
     await message.reply("Угадай число от 1 до 3")
 
-@dp.message_handler()
-async def guess_number(message: types.Message):
+@router.message()
+async def guess_number(message: Message):
     try:
         guessed_number = int(message.text)
         if guessed_number < 1 or guessed_number > 3:
@@ -35,7 +42,9 @@ async def guess_number(message: types.Message):
         await message.reply("Пожалуйста, введите число")
 
 async def main():
-    await dp.start_polling()
+ 
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
+
