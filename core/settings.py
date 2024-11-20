ee491a06fa2b4640889952df2c539619fadc2bdc
koklok123 <lokgom774@gmail.com>
2024-11-20 19:29:18 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
